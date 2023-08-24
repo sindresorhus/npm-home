b2e2f629eae468f6003d619fb0bb5e1f87019ff0
Tommy <tcm257@yahoo.com>
2023-08-23 20:56:27 -0500

    add note to tests

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 import test from 'ava';
 import {execa} from 'execa';
 
+// Tests only checks that opening doesn't return an error, not that the correct page was opened.
+// These have to be manually verified.
+
 const testCli = test.macro(async (t, args = []) => {
 	await t.notThrowsAsync(execa('./cli.js', args));
 });
